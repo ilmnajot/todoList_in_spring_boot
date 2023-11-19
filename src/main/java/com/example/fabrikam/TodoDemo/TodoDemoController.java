581b964d6fb1bebed8398bc024203fe5c1597644
Elbekjon <96689605+ilmnajot@users.noreply.github.com>
2023-11-19 09:51:17 +0500

    todoList

--- src/main/java/com/example/fabrikam/TodoDemo/TodoDemoController.java
+++ src/main/java/com/example/fabrikam/TodoDemo/TodoDemoController.java
@@ -1,37 +1,37 @@
 package com.example.fabrikam.TodoDemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
-@Controller
+@RestController
+@RequestMapping("/api/todos")
 public class TodoDemoController {
 
-    @Autowired
-    private TodoItemRepository repository;
+    private final TodoItemRepository repository;
 
-    @RequestMapping("/")
+    public TodoDemoController(TodoItemRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/")
     public String index(Model model) {
         ArrayList<TodoItem> todoList = (ArrayList<TodoItem>) repository.findAll();
-        //model.addAttribute("items", todoList);
         model.addAttribute("newitem", new TodoItem());
         model.addAttribute("items", new TodoListViewModel(todoList));
         return "index";
     }
 
-    @RequestMapping("/add")
+    @PostMapping("/add")
     public String addTodo(@ModelAttribute TodoItem requestItem) {
         TodoItem item = new TodoItem(requestItem.getCategory(), requestItem.getName());
         repository.save(item);
         return "redirect:/";
     }
 
-    @RequestMapping("/update")
+    @PutMapping("/update")
     public String updateTodo(@ModelAttribute TodoListViewModel requestItems) {
         for (TodoItem requestItem : requestItems.getTodoList() ) {
              TodoItem item = new TodoItem(requestItem.getCategory(), requestItem.getName());
--- src/main/java/com/example/fabrikam/TodoDemo/TodoListViewModel.java
+++ src/main/java/com/example/fabrikam/TodoDemo/TodoListViewModel.java
@@ -1,8 +1,7 @@
 package com.example.fabrikam.TodoDemo;
 
-import java.util.ArrayList;
-
 import javax.validation.Valid;
+import java.util.ArrayList;
 
 public class TodoListViewModel {
 
